25c2a28933822af02cd242e46172cf622e145fb9
Deepankar01 <deepankar01agrawal@gmail.com>
2016-11-27 23:19:27 +0530

    Added the final code for android

--- index.android.js
+++ index.android.js
@@ -6,48 +6,158 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Navigator,
+    StatusBar,
+    Image,
+    Animated,
+    Platform,
+    ScrollView,
 } from 'react-native';
 
+const HEADER_MAX_HEIGHT = 200;
+const HEADER_MIN_HEIGHT = Platform.OS === 'ios' ? 60 : 73;
+const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
+
 export default class parallaxView extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            scrollY: new Animated.Value(0),
+        };
+    }
+
+    _renderScrollViewContent() {
+        const data = Array.from({length: 30});
+        return (
+            <View style={styles.scrollViewContent}>
+                {data.map((_, i) =>
+                    <View key={i} style={styles.row}>
+                        <Text>{i}</Text>
+                    </View>
+                )}
+            </View>
+        );
+    }
+
+
   render() {
+
+      const headerHeight = this.state.scrollY.interpolate({
+          inputRange: [0, HEADER_SCROLL_DISTANCE],
+          outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+          extrapolate: 'clamp',
+      });
+
+      const imageOpacity = this.state.scrollY.interpolate({
+          inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
+          outputRange: [1, 1, 0],
+          extrapolate: 'clamp',
+      });
+      const imageTranslate = this.state.scrollY.interpolate({
+          inputRange: [0, HEADER_SCROLL_DISTANCE],
+          outputRange: [0, -50],
+          extrapolate: 'clamp',
+      });
+
+      const titleScale = this.state.scrollY.interpolate({
+          inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
+          outputRange: [1, 1, 0.8],
+          extrapolate: 'clamp',
+      });
+      const titleTranslate = this.state.scrollY.interpolate({
+          inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
+          outputRange: [0, 0, -8],
+          extrapolate: 'clamp',
+      });
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+        <View style={styles.fill}>
+            <StatusBar
+                translucent
+                barStyle="light-content"
+                backgroundColor="rgba(0, 0, 0, 0.251)"
+            />
+            <ScrollView
+                style={styles.fill}
+                scrollEventThrottle={16}
+                onScroll={Animated.event(
+                        [{nativeEvent: {contentOffset: {y: this.state.scrollY}}}]
+                    )}
+            >
+                {this._renderScrollViewContent()}
+            </ScrollView>
+            <Animated.View style={[styles.header, {height: headerHeight}]}>
+                <Animated.Image
+                    style={[
+                            styles.backgroundImage,
+                            {opacity: imageOpacity, transform: [{translateY: imageTranslate}]},
+                        ]}
+                    source={require('./images/header.jpg')}
+                />
+                <Animated.View
+                    style={[
+                            styles.bar,
+                            {transform: [{scale: titleScale}, {translateY: titleTranslate}]},
+                        ]}
+                >
+                    <Text style={styles.title}>Title</Text>
+                </Animated.View>
+            </Animated.View>
+        </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    fill: {
+        flex: 1,
+    },
+    content: {
+        flex: 1,
+    },
+    header: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: '#03A9F4',
+        overflow: 'hidden',
+    },
+    backgroundImage: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        width: null,
+        height: HEADER_MAX_HEIGHT,
+        resizeMode: 'cover',
+    },
+    bar: {
+        marginTop: Platform.OS === 'ios' ? 28 : 38,
+        height: 32,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        color: 'white',
+        fontSize: 18,
+    },
+    scrollViewContent: {
+        marginTop: HEADER_MAX_HEIGHT,
+    },
+    row: {
+        height: 40,
+        margin: 16,
+        backgroundColor: '#D3D3D3',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
 
 AppRegistry.registerComponent('parallaxView', () => parallaxView);
